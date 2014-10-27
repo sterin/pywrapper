989043992fcb0654594af58ab3235e0dea7741d7
Baruch Sterin <baruchs@gmail.com>
2014-10-26 19:15:19 -0700

    support sub interpreters

--- src/pywrapper_embed.h
+++ src/pywrapper_embed.h
@@ -115,6 +115,122 @@ private:
 	bool _held;
 };
 
+class restore_tstate
+{
+public:
+    restore_tstate()
+    {
+        _ts = PyThreadState_Get();
+    }
+
+    ~restore_tstate()
+    {
+        PyThreadState_Swap(_ts);
+    }
+
+private:
+    PyThreadState* _ts;
+};
+
+class swap_tstate
+{
+public:
+    swap_tstate(PyThreadState* ts)
+    {
+        _ts = PyThreadState_Swap(ts);
+    }
+
+    ~swap_tstate()
+    {
+        PyThreadState_Swap(_ts);
+    }
+
+private:
+
+    PyThreadState* _ts;
+};
+
+
+class thread_state
+{
+public:
+
+    thread_state(PyInterpreterState* interp)
+    {
+        _ts = PyThreadState_New(interp);
+    }
+
+    ~thread_state()
+    {
+        if( _ts )
+        {
+            PyThreadState_Clear(_ts);
+            PyThreadState_Delete(_ts);
+
+            _ts = nullptr;
+        }
+    }
+
+    operator PyThreadState*()
+    {
+        return _ts;
+    }
+
+private:
+
+    PyThreadState* _ts;
+};
+
+class sub_interpreter
+{
+public:
+
+    struct thread_scope
+    {
+        gil_lock _lock;
+        thread_state _state;
+        swap_tstate _swap;
+
+        thread_scope(PyInterpreterState* interp) :
+            _state(interp),
+            _swap(_state)
+        {
+        }
+    };
+
+    sub_interpreter()
+    {
+        restore_tstate restore;
+
+        _ts = Py_NewInterpreter();
+    }
+
+    ~sub_interpreter()
+    {
+        if( _ts )
+        {
+            swap_tstate sts(_ts);
+
+            Py_EndInterpreter(_ts);
+        }
+    }
+
+    PyInterpreterState* interp()
+    {
+        return _ts->interp;
+    }
+
+    operator PyInterpreterState*()
+    {
+        return _ts->interp;
+    }
+
+private:
+
+    PyThreadState* _ts;
+};
+
+
 } // namespace py
 
 #endif // PYTHONWRAPPER_EMBED__H
